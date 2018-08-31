e1701a61819751c5ad87f24947ab9972c3abac32
Michael Andersen <m.andersen@cs.berkeley.edu>
2018-08-31 10:43:15 -0700

    Missed one

--- wave3/__init__.py
+++ wave3/__init__.py
@@ -4,3 +4,5 @@ from .eapi_pb2_grpc import *
 
 WaveBuiltinPSET = "GyAZSVTobuuPkf-YOsxW5shK4pqQfOfnY46GV9UUmbGIpA=="
 WaveGlobalNamespace = "GyDPjRnXnSMBOGW-91fOoEze5e9O3vyAjdIeTgBeb4BHzA=="
+
+WaveBuiltinE2EE = "decrypt"
