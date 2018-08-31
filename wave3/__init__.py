cbddff22f157cfcc54acbdcff660adfb5f6f135f
Michael Andersen <m.andersen@cs.berkeley.edu>
2018-08-31 10:41:18 -0700

    Add builtin constants

--- wave3/__init__.py
+++ wave3/__init__.py
@@ -1,2 +1,6 @@
 from .eapi_pb2 import *
 from .eapi_pb2_grpc import *
+
+
+WaveBuiltinPSET = "GyAZSVTobuuPkf-YOsxW5shK4pqQfOfnY46GV9UUmbGIpA=="
+WaveGlobalNamespace = "GyDPjRnXnSMBOGW-91fOoEze5e9O3vyAjdIeTgBeb4BHzA=="
